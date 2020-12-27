428c115658c57201184b23fe129e7e886e4f923f
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-27 10:11:06 -0500

    add main server code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+pub async fn main() {
+    let ftp_home = std::env::temp_dir();
+    let server = libunftp::Server::with_fs(ftp_home)
+        .greeting("Welcome to my FTP server")
+        .passive_ports(50000..65535);
+
+    server.listen("127.0.0.1:2121").await;
 }
